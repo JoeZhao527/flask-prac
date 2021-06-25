870a51e6ae441c61f20b19ddbd4933fda73935a1
JoeZhao527 <56780675+JoeZhao527@users.noreply.github.com>
2021-06-25 15:10:34 +0800

    Add files via upload

---
+++ Flask_prac/01/market.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+db = SQLAlchemy(app)
+
+class Item(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(length=30), nullable=False)
+    price = db.Column(db.Integer(), nullable=False)
+    barcode = db.Column(db.String(length=12), nullable=False, unique=True)
+    description = db.Column(db.String(length=1024), nullable=False, unique=True)
+    
+    def __repr__(self):
+        return f'Item {self.name}'
+
+@app.route('/')
+def home_page():
+    items = Item.query.all()
+    return render_template('index.html', item_name=items)
